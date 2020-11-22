745bb29b998d8bcc7b899a91e0ff721bc24352b3
Alex Egger <alex.egger96@gmail.com>
2020-11-22 14:34:16 +0100

    Add check for external monitor before setting workspaces

--- .config/bspwm/bspwm.d/workspaces.sh
+++ .config/bspwm/bspwm.d/workspaces.sh
@@ -4,7 +4,10 @@ set -euo pipefail
 function main {
     bspc monitor "DP-4" -d I II III IV V VI VII
     bspc monitor "HDMI-0" -d VIII IX X
-    bspc monitor "DP-3" -d External
+
+    if [ "$(bspc query --monitors | wc -l)" -gt 2 ]; then
+        bspc monitor "DP-3" -d External
+    fi
 }
 
 main "$@"
