669848918327d56d16cb8fb4303ce49a0bdf6f01
Alex Egger <alex.egger@mixed-mode.de>
2020-11-24 09:42:16 +0100

    Add monitor setup for Titanic

--- .config/bspwm/bspwm.d/monitor.sh
+++ .config/bspwm/bspwm.d/monitor.sh
@@ -2,12 +2,14 @@
 set -euo pipefail
 
 function main {
-    xrandr --output "DP-4" --mode 1920x1080 --rate 144.00 --primary
-    xrandr --output "HDMI-0" --mode 1920x1080 --rate 60.00 --right-of "DP-4"
+	if [[ $(hostnamectl --transient) == "Titanic" ]]; then
+		xrandr --output "DP-4" --mode 1920x1080 --rate 144.00 --primary
+		xrandr --output "HDMI-0" --mode 1920x1080 --rate 60.00 --right-of "DP-4"
 
-    # The default state for the external TV screen is off.
-    # Use * and * to enable/disable it.
-    xrandr --output "DP-3" --off
+		# The default state for the external TV screen is off.
+		# Use * and * to enable/disable it.
+		xrandr --output "DP-3" --off
+	fi
 }
 
 main "$@"
