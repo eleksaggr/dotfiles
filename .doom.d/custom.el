adb2988471ebdcfb6baa1006ea061a1443a8c0a5
Alex Egger <alex.egger@mixed-mode.de>
2020-12-18 09:28:48 +0100

    Disable italic comments in code for Doom

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -11,4 +11,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(font-lock-comment-face ((t (:inherit nil :foreground "#56687e"))))
  '(org-headline-done ((t (:foreground "dim gray" :strike-through "dim gray")))))
