80c22c7e9db680437eff15e164c88df215415192
Alex Egger <alex.egger@mixed-mode.de>
2021-02-14 14:32:16 +0100

    Move workspace setup in with monitor setup

--- .config/bspwm/bspwm.d/monitor.sh
+++ .config/bspwm/bspwm.d/monitor.sh
@@ -1,15 +1,41 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-function main {
-	if [[ $(hostnamectl --transient) == "Titanic" ]]; then
-		xrandr --output "DP-4" --mode 1920x1080 --rate 144.00 --primary
-		xrandr --output "HDMI-0" --mode 1920x1080 --rate 60.00 --right-of "DP-4"
+DPPATH=/sys/class/drm/card0-DP-1/status
+function isDPConnected {
+	[[ $(cat "${DPPATH}") == "connected" ]]
+}
 
-		# The default state for the external TV screen is off.
-		# Use * and * to enable/disable it.
-		xrandr --output "DP-3" --off
+function setupWorkspaces {
+	if [[ $(hostnamectl --static) == "Titanic" ]]; then
+		if isDPConnected; then
+			bspc monitor "DP-1" -d I II III IV V
+			bspc monitor "eDP-1" -d VI VII VIII IX X
+		else
+			bspc monitor "eDP-1" -d I II III IV V VI VII VIII IIX IX X
+		fi
+	else
+		bspc monitor "DP-4" -d I II III IV V VI VII
+		bspc monitor "HDMI-0" -d VIII IX X
+
+		if [ "$(bspc query --monitors | wc -l)" -gt 2 ]; then
+			bspc monitor "DP-3" -d External
+		fi
 	fi
+}
+
+function setupMonitors {
+	if [[ $(hostnamectl --transient) == "Titanic" ]]; then
+		xrandr --output "eDP-1" --mode 3840x2160 --primary
+		if isDPConnected; then
+			xrandr --output "DP-1" --mode 3840x2160 --right-of "eDP-1"
+		fi
+	fi
+}
+
+function main {
+	setupMonitors
+	setupWorkspaces
 }
 
 main "$@"
