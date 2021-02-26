63c86c66eaba4858811b32c21ceb597964576d25
Alex Egger <alex.egger96@gmail.com>
2021-02-26 22:39:08 +0100

    bspwm: Remove external monitor setup for Bomboclaat

--- .config/bspwm/bspwm.d/monitor.sh
+++ .config/bspwm/bspwm.d/monitor.sh
@@ -17,10 +17,6 @@ function setupWorkspaces {
 	else
 		bspc monitor "DP-4" -d I II III IV V VI VII
 		bspc monitor "HDMI-0" -d VIII IX X
-
-		if [ "$(bspc query --monitors | wc -l)" -gt 2 ]; then
-			bspc monitor "DP-3" -d External
-		fi
 	fi
 }
 
